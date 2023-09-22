5da94f2b73c455035d5bac57b3544046f9275567
Pascal Collins <pascal.collins@iconnewmedia.de>
2023-09-22 09:23:06 +0200

    removes deprecated settings file and increases version number

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_learningcompanions_mygroups';
-$plugin->release = '0.1.0';
-$plugin->version = 2022102502;
+$plugin->release = '1.0.0';
+$plugin->version = 2023092000;
 $plugin->requires = 2019111800;
 $plugin->dependencies = array('local_learningcompanions' => 2022102501);
